d2753b728e7a5523d4c98a094704886f8f76275c
Rushmerk <57403682+Rushmerk@users.noreply.github.com>
2021-09-30 00:02:51 -0300

    preload() aducionado
    Resolvi o problema dos requests a cada scroll. Agora o array de fotos é baixado ao carregar a pagina

--- js/script.js
+++ js/script.js
@@ -2,38 +2,16 @@ var img = document.createElement("IMG") // pode-se dizer que faz a instancia de 
 quantidade_fotos = 60 // quantidade de fotos dentro da pasta desejada
 ArrayImg = []
 
-function preloadImages(array) { // não faço IDEIA de como essa função funciona
-    if (!preloadImages.list) {
-        preloadImages.list = [];
-    }
-    var list = preloadImages.list;
-    for (var i = 0; i < array.length; i++) {
-        var img = new Image();
-        img.onload = function() {
-            var index = list.indexOf(this);
-            if (index !== -1) {
-                // remove image from the array once it's loaded
-                // for memory consumption reasons
-                list.splice(index, 1);
-            }
-        }
-        list.Push(img);
-        img.src = array[i];
+function preload() { // faz o preload das imagens. Ta funcionando perfeitamente
+    for (let i = 0; i <= quantidade_fotos; i++) {
+        _base = verificarTamanho(i) // 2 console.log aqui dentro
+        ArrayImg[i] = new Image();
+        ArrayImg[i].src = _base + i + ".jpg"
+        console.log(ArrayImg[i].src)
     }
 }
 
-window.onload = function() {
-    console.log("carregou a pagina")
-    for (let i = 1; i < quantidade_fotos; i++) {
-        console.log("window.onload iniciou o loop")
-        _base = verificarTamanho(i) // 2 console.log aqui dentro
-        console.log("window.onload valor i: " + i.toString())
-        console.log(_base + i + ".jpg")
-        ArrayImg.push(_base + i + ".jpg")
-        console.log(ArrayImg.length)
-    }
-    preloadImages(ArrayImg)
-}
+preload()
 
 window.onscroll = function() { //tudo começa com o scroll do usuario
     movimentoAtual = parseInt(window.scrollY / 100) // Coleta a posição atual do usuario no eixo Y da janela. Esse int representa a foto
@@ -41,24 +19,20 @@ window.onscroll = function() { //tudo começa com o scroll do usuario
 }
 
 function next(movimentoAtual) { //parametro recebido pela window.onscroll
-    console.log("função next")
+    _valor = movimentoAtual.toString()
+    _base = verificarTamanho(_valor)
 
-    valor = movimentoAtual.toString()
-    console.log("valor: " + valor + typeof valor)
-
-    _base = verificarTamanho(valor)
-
-    img.src = _base + valor + ".jpg" // Definindo a string do img.src
+    img.src = _base + _valor + ".jpg" // Definindo a string do img.src
     console.log(img)
 
-    if (valor == 0) { // caso o window.onscroll retorne 0. Se não for feito, nenhuma imagem será exibida
-        console.log("ENTROU NO IFFFFFFFFFFFFFFFF")
+    if (_valor == 0) { // caso o window.onscroll retorne 0. Se não for feito, nenhuma imagem será exibida
+        console.log("COMEÇOOOOOOOO")
         img.src = "img/test0001.jpg"
         console.log(img)
         document.getElementById('image').appendChild(img) // Definindo a imagem
     }
-    if (valor > quantidade_fotos) { // Caso o window.onscroll retorne um valor maior do que a quantidade de fotos
-        console.log("CHEGOU NO FINAAAAAAAAAAAAAAAAALLL")
+    if (_valor > quantidade_fotos) { // Caso o window.onscroll retorne um valor maior do que a quantidade de fotos
+        console.log("FINAAAAAAAAAAAAAAAAALLL")
         img.src = _base + quantidade_fotos + ".jpg"
         console.log(img)
         document.getElementById('image').appendChild(img) // Coloca a imagem na div correspondente
@@ -68,7 +42,6 @@ function next(movimentoAtual) { //parametro recebido pela window.onscroll
 
 function verificarTamanho(valor) { //estrutura necessaria para se ajustar a quantidade de arquivos
 
-    console.log("verificarTamanho: verificando tamanho do valor")
     this.valor = valor.toString()
     switch (this.valor.length) {
         case 1:
@@ -83,6 +56,6 @@ function verificarTamanho(valor) { //estrutura necessaria para se ajustar a quan
         default:
             break;
     }
-    console.log("verificarTamanho: base " + base_verificada)
+    // console.log("verificarTamanho: base " + base_verificada)
     return base_verificada;
 }
